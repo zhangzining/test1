b4d52281aa9c1984327d5a1318834a7275036b90
zhangzining <zhangzining1999@gmail.com>
2018-04-17 11:52:05 +0800

    2018/4/17

--- 8.c
+++ 8.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 void copy_char(char a[],char b[]){
-int k=0;
-for(int i=1;a[i]!="\0";i++){
-	
-	if(a[i]>65&&a[i]<90){
+int k=0,i;
+for(i=0;i<strlen(a);i++){
+	if(a[i]>65 && a[i]<90){
 		b[k]=a[i];
-		k++;	
+		k+=1;
+	}
+	else if(a[i]=='\0'){
+		return;
 	}
 }
 
 }
 int main(){
-char a[10]={0},b[10]={0};
+printf("please input max 10 char:\n");
+char a[]={},b[10]={};
 scanf("%s",a);
 copy_char(a,b);
-printf("%s",b);
+printf("%s\n",b);
 return 0;
 }
--- a.md
+++
@@ -1,2 +0,0 @@
-this is my c tests
-another line
